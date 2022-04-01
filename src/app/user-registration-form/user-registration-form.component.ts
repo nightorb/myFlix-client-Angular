32665c20e720da8187792e8a79104310ba029d7c
nightorb <ana.nath@gmx.de>
2022-04-02 00:21:35 +0200

    create user-registration component

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+// used to close the dialog on success
+import { MatDialogRef } from '@angular/material/dialog';
+// used to display notifications to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+// brings in the API calls
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  // the selector makes it possible to use this component in HTML files like this:
+  // <app-user-registration-form></app-user-registration-form>
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  // @Input decorator defines the component's input (here: the user data)
+  // the userData object will be passed into the API call in the registerUser function
+  @Input() userData = {
+    Username: '',
+    Password: '',
+    Email: '',
+    Birthday: ''
+  };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
+
+  // called once the component has received all its inputs (it's data-bound properties)
+  // from the calling component (the real-life user)
+  ngOnInit(): void {
+  }
+
+  // function that sends form input to the backend
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+      // logic for successful user registration (to be implemented)
+
+      // closes the modal on success
+      this.dialogRef.close();
+      console.log(result);
+      this.snackBar.open('user registered successfully', 'OK', {
+        duration: 2000
+      });
+    }, (result) => {
+      console.log(result);
+      this.snackBar.open(result, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+}
