01f565a315728f4e036e4344238c5117fa7da565
nightorb <ana.nath@gmx.de>
2022-04-06 01:26:53 +0200

    update movie-card component
    added basic structure of functions for adding/removing favorite movies, and opening director/genre/synopsis cards

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { DirectorCardComponent } from '../director-card/director-card.component';
+import { GenreCardComponent } from '../genre-card/genre-card.component';
+import { SynopsisCardComponent } from '../synopsis-card/synopsis-card.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -7,10 +12,15 @@ import { FetchApiDataService } from '../fetch-api-data.service';
   styleUrls: ['./movie-card.component.scss']
 })
 export class MovieCardComponent implements OnInit {
+  username = localStorage.getItem('user');
+  user: any = {};
   // movies returned from the API call will be kept here
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog) { }
 
   // lifecycle hook; called when Angular is done creating the component
   ngOnInit(): void {
@@ -23,6 +33,40 @@ export class MovieCardComponent implements OnInit {
       this.movies = response;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  addFavoriteMovies(MovieID: string, Title: string): void {
+    this.fetchApiData.addFavoriteMovie(MovieID).subscribe((res: any) => {
+      this.snackBar.open(`${Title} has been added to your favorites`, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  removeFavoriteMovies(MovieID: string, Title: string): void {
+    this.fetchApiData.removeFavoriteMovie(MovieID).subscribe((res: any) => {
+      this.snackBar.open(`${Title} has been removed from your favorites`, 'OK', {
+        duration: 2000
+      });
+    });
+  }
+
+  openDirectorDialog(): void {
+    this.dialog.open(DirectorCardComponent, {
+      width: '500px'
+    });
+  }
+
+  openGenreDialog(): void {
+    this.dialog.open(GenreCardComponent, {
+      width: '500px'
+    });
+  }
+
+  openSynopsisDialog(): void {
+    this.dialog.open(SynopsisCardComponent, {
+      width: '500px'
     });
   }
 }
